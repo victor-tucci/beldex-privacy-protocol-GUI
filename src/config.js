3a847c4856ce884b33f536537536c845547d0261
victor-tucci <victor.tucci@beldex.io>
2024-01-24 13:22:32 +0530

    updated testnet contract

--- src/config.js
+++ src/config.js
@@ -2,18 +2,18 @@ const config = {
     mnemonic: "must antenna drop draw repeat crumble excuse skin stumble either mask plate",
     deployed : {
         // sarath 7.6(mainnet)
-        'Utils': "0x840d224C6930E23a2bE1f933E209334c22dEA0db",
-        'BeldexIP': "0x4C669D6534955FCdaE66450125133E3755908193",
-        'BeldexRedeem': "0x644B7B737C50491025ed746af023f16728a8bceC",
-        'BeldexTransfer': "0x50c55FBFf8031800A8602023CeeF4FE47E93952c",
-        'BeldexMAT': "0xA8018C0d83aA999d2DAE60A4A76a6eCc42c872A0",
+        // 'Utils': "0x840d224C6930E23a2bE1f933E209334c22dEA0db",
+        // 'BeldexIP': "0x4C669D6534955FCdaE66450125133E3755908193",
+        // 'BeldexRedeem': "0x644B7B737C50491025ed746af023f16728a8bceC",
+        // 'BeldexTransfer': "0x50c55FBFf8031800A8602023CeeF4FE47E93952c",
+        // 'BeldexMAT': "0xA8018C0d83aA999d2DAE60A4A76a6eCc42c872A0",
         
         // saravanan(update) matictestnet
-        // 'Utils': "0x4d14cd36e6Faa727bcEF8F2b797F7C447B302AFE",
-        // 'BeldexIP': "0x59A0aDef3EC7c9C142B43f6A60b84a032B6fA1DE",
-        // 'BeldexRedeem': "0x53B324B84acd070b8e546C492a8CD89cc906D18D",
-        // 'BeldexTransfer': "0x9094f0958EbFcb843258e06d1E7F1eb6508f45e5",
-        // 'BeldexMAT': "0xC2a40d663017A6F9ce31844Cd2865273f172194f",
+        'Utils': "0x1df0B9786d99725e8C743e77bCbe1ecDEc51C00E",
+        'BeldexIP': "0xfB5a661d841957795B8FF3555706C608958deF8B",
+        'BeldexRedeem': "0xDb777E30411371ABb0cfBfdCb856902393F69dDb",
+        'BeldexTransfer': "0x5085a3F92caF13CDAFd1AECc2017F3eeBCF423cb",
+        'BeldexMAT': "0x271AD740D6abEbEe480173C5Edd32988FA964783",
 
     }
     // deployed: {
